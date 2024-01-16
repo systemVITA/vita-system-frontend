ffef867cc6eb9b9c83dae84db0131c2759458736
nervaljunior <nerval.junior@discente.ufma.br>
2024-01-16 18:16:08 -0300

    refactoring from index

--- VITA_REFACTORING/src/pages/index.tsx
+++ VITA_REFACTORING/src/pages/index.tsx
@@ -34,8 +34,6 @@ export default function Home() {
 
   return (
     
-
-    
     <div className='flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900'>
       <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800'>
         <div className='flex flex-col overflow-y-auto md:flex-row'>
